b2d7b08c19146877c69a262309b1f64a0ce18709
joshsutton47 <56270887+joshsutton47@users.noreply.github.com>
2022-04-11 16:41:39 -0500

    Halfway-Completed Game (4.11.22)
    Created the unity project and finished everything through object pooling

---
+++ SpaceShooterUnity/Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,80 @@
+/**** 
+ * Created by: Josh Sutton
+ * Date Created: April 11, 2022
+ * 
+ * Last Edited by: N/A
+ * Last Edited: April 11, 2022
+ * 
+ * Description: object pool
+ ****/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    /* VARIABLES */
+    static public ObjectPool POOL;
+    #region Pool Singleton
+    //Check to make sure only one gm of the GameManager is in the scene
+    void CheckPOOLIsInScene()
+    {
+        //Check if instnace is null
+        if (POOL == null)
+        {
+            POOL = this; //set SHIP to this game object
+        }
+        else //else if SHIP is not null send an error
+        {
+            Debug.LogError("POOL.Awake() - Attempeeted to assign second ObjectPool.POOL");
+        }
+    }//end CheckSHIPIsInScene()
+    #endregion
+
+    private Queue<GameObject> projectiles = new Queue<GameObject>(); //the queue for the projectiles
+    [Header("Pool Settings")]
+    public GameObject projectilePrefab; //projectile prefab
+    public int poolStartSize = 5; //starting size of pool
+
+    //Awake is called when the game loads (before starting). Awake only once during the lifetime of the script instance.
+    
+     void Awake()
+     {
+        CheckPOOLIsInScene();
+    }//end Awake()
+     // Start is called before the first frame update
+    void Start()
+    {
+
+        for (int i = 0; i < poolStartSize; i++)
+        {
+            GameObject projectileGO = Instantiate(projectilePrefab); //create prefab instance
+            projectiles.Enqueue(projectileGO); //add to queue
+            projectileGO.SetActive(false);//hide projectile
+        }//end for(int i = 0; i < poolStartSize; i++)
+    }//end Start()
+
+    public GameObject GetObject()
+    {
+        //If there are objects in the pool
+        if (projectiles.Count > 0)
+        {
+            GameObject gObject = projectiles.Dequeue(); //remove from queue
+            gObject.SetActive(true); //enable
+            return gObject; //return object
+        }
+        else
+        {
+            Debug.LogWarning("Out of objects, reloading...");
+            return null; //return null
+        }//end if(projecitles.Count > 0)
+    }//end GetProjectile()
+
+    public void ReturnObject(GameObject gObject)
+    {
+        projectiles.Enqueue(gObject); //add back to queue
+        gObject.SetActive(false); //disable
+    }//end ReturnProjectile()
+
+}
---
+++ SpaceShooterUnity/Assets/Scripts/Projectile.cs
@@ -0,0 +1,32 @@
+/**** 
+ * Created by: Josh Sutton
+ * Date Created: April 11, 2022
+ * 
+ * Last Edited by: N/A
+ * Last Edited: April 11, 2022
+ * 
+ * Description: Controls projectiles
+****/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    /*** Variables***/
+    private BoundsCheck bndCheck; //reference to the bounds check
+    private void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }//end Awake()
+     // Update is called once per frame
+    void Update()
+    {
+        //if off screen up, destroy
+        if (bndCheck.offUp)
+        {
+            gameObject.SetActive(false); //set the project to deactivate and return to pool
+            bndCheck.offUp = false; //reset the BoundsCheck offUp boolean
+        }
+    }//end Update()
+}
